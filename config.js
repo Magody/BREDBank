2048c03b18108ced77514fa549a971c965ccc6d0
Andres Roman <andres11391@hotmail.com>
2020-02-09 15:12:36 -0500

    SSL certificates X509 self-signed added

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+
 const config = {
+
     dbUrl: process.env.DB_URL || 'mongodb://localhost:27017/bred_bank',  //usa variable de entorno o la mia
-    host: process.env.HOST || 'http://localhost',
+    host: process.env.HOST || 'https://localhost',
     port: process.env.PORT || 3002,
     publicRoute: process.env.PUBLIC_ROUTE || '/bank',
     files: process.env.PUBLIC_ROUTE_FILES || '/files',
+    key: fs.readFileSync('./keys/server.key'),
+    cert: fs.readFileSync('./keys/server.crt')
     
 }
 
