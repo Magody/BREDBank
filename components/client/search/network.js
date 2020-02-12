83693735c8c0c65ed0e24c82fa7b6e6b187cec13
EioFlc14 <48327753+EioFlc14@users.noreply.github.com>
2020-02-12 18:53:10 -0500

    avances

--- components/client/search/network.js
+++ components/client/search/network.js
@@ -13,14 +13,28 @@ router.post('/', function(request, response){
     console.log('search desde boton search principal:' , request.body);
     controllerTransaction.getTransaction(request.body.numberAccountS)
     .then((transactions)=>{
-        console.log('transactions:', transactions);
-        console.log('transactions[0].movement:', transactions[0].movement);
-        response.redirect('/client/search/?transactions=' + transactions[0].movement);
+         transactions.forEach(
+
+
+             element => console.log('transaction element:', element)
+         );
+
+        response.send('<form id="transactions" method="post" action="/client/search">' + 
+
+        '<script type="text/javascript" > $("#search-table td").parent().remove(); var trHTML = "";' + transactions +'.forEach( function(valor, indice, array){trHTML += <tr id ="tr_indice" ><td style=" margin: 3px; padding: 3px; border: 1px solid"> valor["originAccount"] </td><td style=" margin: 3px; padding: 3px; border: 1px solid"> valor["destinationAccount"] </td><td style=" margin: 3px; padding: 3px; border: 1px solid"> valor["amount"] </td></tr>  }); $("#user-table").append(trHTML); </script>');
+
+        //response.redirect('/client/search/?transactions=' + transactions);
+        // res.send('<form id="redirectVerification" method="post" action="/client/verification">' +
+        //            '<input type="hidden" name="user" value="'+ cliente.user+ '">' +
+        //            '<input type="hidden" name="email" value="'+ cliente.email + '">' +
+        //            '</form>' +
+        //            '<script>document.getElementById("redirectVerification").submit()</script>');
     })
     .catch((e)=>{
         res.error(request, response, "Error interno", 500, e)
     })
 
 });
+
 
 module.exports = router;
